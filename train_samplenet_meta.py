fa436b9e55953307c850fae82c5e83fca2767b5d
Tim Cheng <ttchengab@connect.ust.hk>
2022-07-23 11:10:17 +0100

    Increase Readability

--- train_samplenet_meta.py
+++ train_samplenet_meta.py
@@ -14,8 +14,8 @@ import torch
 import torch.nn as nn
 from models import pointnet_cls
 from models import pcn
-from models import samplenet as samplenet
 from models import pointnet_retrieval
+from models import samplenet as samplenet
 from data_utils.ModelNetMetaLoader import ModelNetDataLoader
 from tqdm import tqdm
 
@@ -73,50 +73,18 @@ sampler = samplenet.SampleNet(
 )
 sampler = sampler.cuda()
 
+"""Initialize meta learning models"""
+task_models = []
+
+# Add Classification Models
 num_class = args.num_category
 model = pointnet_cls
-
-# Testing on the original network
-# test_network = model.get_model(num_class, normal_channel=args.use_normals)
-# for param in test_network.parameters():
-#     param.requires_grad = False
-# if not args.use_cpu:
-#     test_network = test_network.cuda()
-# checkpoint = torch.load('log/classification/2021-11-19_07-05/checkpoints/best_model.pth')
-# print('testing network is suboptimal')
-# test_network.load_state_dict(checkpoint['model_state_dict'])
-# test_network.eval()
-#
-# classifier = model.get_model(num_class, normal_channel=args.use_normals)
-# # classifier.apply(inplace_relu)
-# criterion = model.get_loss()
-# checkpoint = torch.load('log/classification/2021-11-19_07-05/checkpoints/best_model.pth')
-# # checkpoint = torch.load('log/classification/2021-11-19_07-05/checkpoints/best_model.pth')
-# print('Loading the best model')
-# classifier.load_state_dict(checkpoint['model_state_dict'])
-# for param in classifier.parameters():
-#     param.requires_grad = False
-# if not args.use_cpu:
-#     classifier = classifier.cuda()
-# classifier.eval()
-
-# pcnet = pcn.PCN()
-# pcnet = pcnet.cuda()
-# checkpoint = torch.load('pcn_dir')
-# print('Loading the best pcn model')
-# pcnet.load_state_dict(checkpoint['model_state_dict'])
-# for param in pcnet.parameters():
-#     param.requires_grad = False
-# pcnet.eval()
-
-"""Initialize meta learning models"""
-checkpoints_dir = ['ptnet_cls_1','ptnet_cls_2','ptnet_cls_3']
-num_tasks = len(checkpoints_dir)
-task_models = []
-for i in range(num_tasks):
+checkpoints_dir = ['ptnet_cls_1','ptnet_cls_2','ptnet_cls_3'] # checkpoint names for classification
+num_tasks_cls = len(checkpoints_dir)
+for i in range(num_tasks_cls):
     classifier = model.get_model(num_class, normal_channel=args.use_normals)
-    criterion = model.get_loss()
-    checkpoint = torch.load('log/classification/' + checkpoints_dir[i] + '/checkpoints/best_model.pth')
+    criterion_cls = model.get_loss()
+    checkpoint = torch.load('log/classification/' + checkpoints_dir[i] + '/checkpoints/best_model.pth') # Update checkpoint directory here
     classifier.load_state_dict(checkpoint['model_state_dict'])
     for param in classifier.parameters():
         param.requires_grad = False
@@ -124,15 +92,14 @@ for i in range(num_tasks):
         classifier = classifier.cuda()
     classifier.eval()
     task_models.append(classifier)
-    # task_losses.append(criterion)
+    # task_losses.append(criterion_cls)
 
-"""ADD EVERYTHING INTO ONE TASK MODEL LIST WITH CORRESPONDING LOSSES"""
-# TODO: Finish Loading All PCNs
-checkpoints_dir = ['pcn_1','pcn_2','pcn_3']
-num_tasks = len(checkpoints_dir)
-for i in range(num_tasks):
+# Add Reconstruction Models
+checkpoints_dir = ['pcn_1','pcn_2','pcn_3'] #checkpoint names for reconstruction
+num_tasks_rec = len(checkpoints_dir)
+for i in range(num_tasks_rec):
     pcn_joint = pcn.PCN()
-    checkpoint = torch.load('log/samplenet_meta/' + checkpoints_dir[i] + '/best.pth')
+    checkpoint = torch.load('log/samplenet_meta/' + checkpoints_dir[i] + '/best.pth') # Update checkpoint directory here
     pcn_joint.load_state_dict(checkpoint['model_state_dict'])
     for param in pcn_joint.parameters():
         param.requires_grad = False
@@ -140,15 +107,14 @@ for i in range(num_tasks):
         pcn_joint = pcn_joint.cuda()
     pcn_joint.eval()
     task_models.append(pcn_joint)
-    # task_losses.append()
 chamfer_loss = pcn.ChamferDistanceLoss()
-#
-"""Add all the shape retrieval models"""
-checkpoints_dir = ['shape_ret1','shape_ret2','shape_ret3']
-num_tasks = len(checkpoints_dir)
-for i in range(num_tasks):
+
+# Add Shape Retrieval Models
+checkpoints_dir = ['shape_ret1','shape_ret2','shape_ret3'] #checkpoint names for shape retrieval
+num_tasks_ret = len(checkpoints_dir)
+for i in range(num_tasks_ret):
     retrievalnet_joint = pointnet_retrieval.get_model(normal_channel=args.use_normals)
-    checkpoint = torch.load('log/shape_retrieval/' + checkpoints_dir[i] + '/checkpoints/best_model.pth')
+    checkpoint = torch.load('log/shape_retrieval/' + checkpoints_dir[i] + '/checkpoints/best_model.pth') # Update checkpoint directory here
     retrievalnet_joint.load_state_dict(checkpoint['model_state_dict'])
     for param in retrievalnet_joint.parameters():
         param.requires_grad = False
@@ -156,16 +122,10 @@ for i in range(num_tasks):
         retrievalnet_joint = retrievalnet_joint.cuda()
     retrievalnet_joint.eval()
     task_models.append(retrievalnet_joint)
-    # task_losses.append()
 bceloss = nn.BCELoss()
-#
-# # TODO: Add training with shape retrieval
-
-
 
 
 num_tasks = len(task_models)
-
 maml_samplers = []
 for i in range(num_tasks):
     maml_sampler = samplenet.SampleNet(
@@ -184,18 +144,16 @@ for i in range(num_tasks):
 """For inference time behavior, set sampler.training = False."""
 sampler.training = True
 
-"""optimizer"""
+"""Meta-Sampler Optimizer"""
 optimizer = torch.optim.Adam(
     sampler.parameters(),
     lr=0.001,
     betas=(0.9, 0.999),
     eps=1e-08,
 )
-# onet_optimizer = torch.optim.Adam(onet.parameters(), lr=0.0001)
 
 """Create Ckpt dir"""
-saving_dir = 'log/samplenet_meta_all/'+str(out_points)+'_'+str(num_tasks)+'all_24_'+datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+'/'
-# model_dir = 'ckpts/MixupVis'+datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+'/'
+saving_dir = 'log/samplenet_meta_all/'+str(out_points)+'_'+str(num_tasks)+datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+'/'
 os.makedirs(saving_dir)
 
 """Training routine."""
@@ -206,14 +164,15 @@ num_steps = 5
 for epoch in range(EPOCHS):
     sampler.training = True
     sampler.train()
-    # onet.train()
+
     if epoch > 50:
         lr *= 0.1
+
     for idx, (points, diff_points, target) in enumerate(tqdm(trainDataLoader)):
 
         orig_points = points.clone().detach()
         orig_points = orig_points.transpose(2, 1)
-        diff_points = diff_points.transpose(2,1)
+        diff_points = diff_points.transpose(2, 1)
         points = points.data.numpy()
         points = provider.random_point_dropout(points)
         points[:, :, 0:3] = provider.random_scale_point_cloud(points[:, :, 0:3])
@@ -238,11 +197,11 @@ for epoch in range(EPOCHS):
             for j in range(num_steps):
                 simp_pc, proj_pc = maml_samplers[i](points)
 
-                if i < 3:
+                if i < num_tasks_cls: #num_tasks_cls should be 3
                     proj_pc = proj_pc.transpose(2, 1)
                     pred, trans_feat = task_models[i](proj_pc)
-                    loss = criterion(pred, target.long(), trans_feat)
-                elif i < 6:
+                    loss = criterion_cls(pred, target.long(), trans_feat)
+                elif i < num_tasks_cls + num_tasks_rec: #num_tasks_cls + num_tasks_rec should be 6
                     predicted_points = task_models[i](proj_pc)
                     loss = chamfer_loss(predicted_points['coarse_output'], points)
                 else:
@@ -259,7 +218,7 @@ for epoch in range(EPOCHS):
                 loss.backward()
                 maml_optimizer.step()
 
-        # do the outside update
+        # Outside Update
         grads = ''
         sum_losses = 0.
         for i in range(len(task_models)):
@@ -268,7 +227,7 @@ for epoch in range(EPOCHS):
             if i < 3:
                 proj_pc = proj_pc.transpose(2, 1)
                 pred, trans_feat = task_models[i](proj_pc)
-                sum_losses += criterion(pred, target.long(), trans_feat)
+                sum_losses += criterion_cls(pred, target.long(), trans_feat)
             elif i < 6:
                 predicted_points = task_models[i](proj_pc)
                 sum_losses += chamfer_loss(predicted_points['coarse_output'], points)
@@ -288,9 +247,8 @@ for epoch in range(EPOCHS):
                 grads += torch.autograd.grad(sum_losses, maml_samplers[i].parameters())
 
 
-        # add in projection and simplification_loss
+        # Add in projection and simplification_loss
         simp_pc, proj_pc = sampler(points)
-        # bceloss = criterion(pred, target.long(), trans_feat)
         simplification_loss = sampler.get_simplification_loss(
                 points, simp_pc, out_points
         )
@@ -301,7 +259,7 @@ for epoch in range(EPOCHS):
             print("Epoch {}/{} Sum_loss is {}, simplification_loss is {}, projection_loss is {}"\
                                     .format(epoch, EPOCHS, sum_losses, simplification_loss, projection_loss))
 
-        # update sampler
+        # update sampler (task first followed by simplification and projection loss)
         for p, grad in zip(sampler.parameters() ,grads):
             p.data.sub_(grad * lr)
         optimizer.zero_grad()
